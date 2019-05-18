0f405f01dce0a89bd69f0c61a765249a50ecb690
tendo19 <tendo19@19tendo.punahou.edu>
2019-05-17 14:24:31 -1000

    worked on array
    added struct, array, doesn't work yet

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -5,25 +5,38 @@
 
 #include <MeggyJrSimple.h>
 
+struct Point 
+{
+  int x;
+  int y;
+};
+Point s1 = {4,4};
+Point s2 = {3,4};
+Point s3 = {2,4};
+Point s4 = {1,4};
+Point s5 = {0,4};
+Point snakeArray[64] = {s1};
+
+
 int x,y;
 int color;
 int xapple;
 int yapple;
 boolean gotApple=false;
-int dir;
+int dir = 0;
+int applesEaten = 0;
+int setAuxLEDs = applesEaten - 1;
+int marker = 1;
 
 void setup() 
 {
   MeggyJrSimpleSetup();
-  x=0;
-  y=3;
-  color=1;
-  xapple=random(8);
-  yapple=random(8);
-  dir=0;
+  x = 4;
+  y = 4;
+  color = 1;
+  xapple = random(8);
+  yapple = random(8);
 }
-
-
 
 
 void loop() 
@@ -31,48 +44,105 @@ void loop()
  drawsnake();
  spawn();
  moveapple();
- movesnake();
  direction();
+ movesnake();
  borders();
- 
+ eating();
+
  DisplaySlate();
  ClearSlate(); 
+ delay(100);
 }
 
 
-
-
-void direction()
+void updatesnake()
 {
-  if (dir==0)
+  for (int i = marker - 1; i > 0; i--)
   {
-    y++;
-    delay(500);
-  }
-  if (dir==90)
-  {
-    x--;
-    delay(500);
-  }
-  if (dir==180)
-  {
-    y--;
-    delay(500);
-  }
-  if (dir==270)
-  {
-    x++;
-    delay(500);
+    snakeArray[i].x = snakeArray[i-1].x;
+    snakeArray[i].y = snakeArray[i-1].y;    
   }
 }
 
+
+void movesnake()
+{
+  CheckButtonsDown();
+  if (Button_Left)
+  {
+    dir = 90;
+  }
+  if (Button_Right)
+  {
+    dir = 270;
+  }
+  if (Button_Up)
+  {
+    dir = 0;
+  }
+  if (Button_Down)
+  {
+    dir = 180;
+  }
+}
 
 
 
 void drawsnake()
 {
- DrawPx(x,y,Blue); 
+ if (gotApple == true)
+ {
+  marker++;
+ }
+ 
+ for (int i = 0; i < marker; i++)
+ {
+   DrawPx(snakeArray[i].x,snakeArray[i].y,Blue); 
+ }
 }
+
+
+
+
+void followdot()
+{
+  Point s1 = {3,4};
+  Point s2 = {4,4};
+}
+
+
+
+void eating()
+{
+  if (gotApple == true)
+  {
+    applesEaten++;
+  }
+  setAuxLEDs = applesEaten - 1;
+}
+
+
+
+void direction()
+{
+  if (dir == 0)
+  {
+    y++;
+  }
+  if (dir == 90)
+  {
+    x--;
+  }
+  if (dir == 180)
+  {
+    y--;
+  }
+  if (dir == 270)
+  {
+    x++;
+  }
+}
+
 
 
 
@@ -87,40 +157,16 @@ void spawn()
 
 void moveapple()
 {
-  if (x==xapple && y==yapple)
+  if (x == xapple && y == yapple)
   {
-    gotApple=true;
+    gotApple = true;
   }
-  if (gotApple==true)
+  if (gotApple == true)
   {
    Tone_Start(ToneDs5,100);
-   xapple=random(8);
-   yapple=random(8);
-   gotApple=false; 
-  }
-}
-
-
-
-
-void movesnake()
-{
-  CheckButtonsPress();
-  if (Button_Left)
-  {
-    dir=90;
-  }
-  if (Button_Right)
-  {
-    dir=270;
-  }
-  if (Button_Up)
-  {
-    dir=0;
-  }
-  if (Button_Down)
-  {
-    dir=180;
+   xapple = random(8);
+   yapple = random(8);
+   gotApple = false; 
   }
 }
 
@@ -129,20 +175,20 @@ void movesnake()
 
 void borders()
 {
-  if (x<0)
+  if (x < 0)
   {
-    x=7;
+    x = 7;
   }
-  if (x>7)
+  if (x > 7)
   {
-    x=0;
+    x = 0;
   }
-   if (y<0)
+   if (y < 0)
   {
-    y=7;
+    y = 7;
   }
-  if (y>7)
+  if (y > 7)
   {
-    y=0;
+    y = 0;
   }  
 }
